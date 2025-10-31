969ecaaa358e983b7b70009f836b919373dfada4
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2025-10-31 22:08:22 +0700

    fix trending

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,59 +12,54 @@ class HomeController
     public function index()
     {
         $allowedCategories = ['Politics','Finance','Health & Lifestyle','Edu/Tech','Technology'];
-
         $usedIds = collect();
 
         $hero = Article::with('category')
             ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
+            ->whereNotNull('slug')->where('slug','!=','')
             ->orderByDesc('views')
-            ->take(3)
-            ->get();
+            ->take(3)->get();
         $usedIds = $usedIds->merge($hero->pluck('id'));
 
         $breaking = Article::with('category')
             ->breakingToday()
             ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
             ->whereNotIn('id', $usedIds)
-            ->latest('created_at')
-            ->take(4)
-            ->get();
+            ->whereNotNull('slug')->where('slug','!=','')
+            ->latest('created_at')->take(4)->get();
         $usedIds = $usedIds->merge($breaking->pluck('id'));
 
         $featured = Article::with('category')
             ->where('is_featured', true)
             ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
             ->whereNotIn('id', $usedIds)
-            ->latest('created_at')
-            ->take(5)
-            ->get();
+            ->whereNotNull('slug')->where('slug','!=','')
+            ->latest('created_at')->take(5)->get();
         $usedIds = $usedIds->merge($featured->pluck('id'));
 
         $side = Article::with('category')
             ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
             ->whereNotIn('id', $usedIds)
-            ->latest('created_at')
-            ->take(4)
-            ->get();
+            ->whereNotNull('slug')->where('slug','!=','')
+            ->latest('created_at')->take(4)->get();
         $usedIds = $usedIds->merge($side->pluck('id'));
-
-        $latest = Article::with('category')
-            ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
-            ->whereNotIn('id', $usedIds)
-            ->latest('created_at')
-            ->take(13)
-            ->get();
 
         $trending = Article::with('category')
             ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
             ->whereNotIn('id', $usedIds)
+            ->whereNotNull('slug')->where('slug','!=','')
             ->trendingScore(7)
-            ->take(5)
-            ->get();
-        $usedIds = $usedIds->merge($side->pluck('id'));
+            ->take(5)->get();
+
+        $usedIds = $usedIds->merge($trending->pluck('id'));
+
+        $latest = Article::with('category')
+            ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
+            ->whereNotIn('id', $usedIds)
+            ->whereNotNull('slug')->where('slug','!=','')
+            ->latest('created_at')->take(13)->get();
 
         $tags = Tag::take(20)->get();
-
         $categories = Category::withCount('articles')
             ->whereIn('name', $allowedCategories)->take(5)->get();
 
@@ -72,19 +67,10 @@ class HomeController
             ->whereHas('category', fn($q) => $q->whereIn('name', $allowedCategories))
             ->whereNotIn('id', $usedIds ?? [])
             ->popularScore(30, commentsWeight: 3.0, decay: 1.2)
-            ->take(3)
-            ->get();
+            ->take(3)->get();
 
         return view('news.index', compact(
-            'hero',
-            'breaking',
-            'featured',
-            'side',
-            'latest',
-            'trending',
-            'tags',
-            'categories',
-            'popular'
+            'hero','breaking','featured','side','latest','trending','tags','categories','popular'
         ));
     }
 }
