b5882c43f46d8e0dc3a70bf13643200c03cc3109
Rafi Ramadhan <rafiramadhanarr17@gmail.com>
2026-02-28 10:54:23 +0700

    fix: share link on whatsapp

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,10 +8,10 @@
     <meta content="Free HTML Templates" name="description">
 
     <!-- Favicon -->
-     <link type="image/png" href="https://fypmedia.id/assets/backoffice/media/logos/FYPLOGO.png" rel="icon">
+    <link type="image/png" href="https://fypmedia.id/assets/backoffice/media/logos/FYPLOGO.png" rel="icon">
 
     <meta property="og:image" content="https://beritadunia.fypmedia.id/img/logo.png">
-    <meta property="og:description" content="Merangkum berita seputar dinamika politik, mulai dari kebijakan daerah, isu internasional, hingga opini.">
+    <meta property="og:description" content="Menghadirkan artikel dan liputan terkurasi dari berbagai isu global dan nasional.">
     <meta property="og:title" content="Berita Dunia">
     <meta property="og:url" content="https://beritadunia.fypmedia.id/">
     <meta property="og:type" content="website">
